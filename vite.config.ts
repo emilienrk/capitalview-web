2f3b72830e53a72092a5d5e5e1c3c753688fa92e
emilienrk <emilien.roukine@proton.me>
2026-03-01 09:02:13 +0100

    refactor(core): changes sources of thruth of api version

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
+import { readFileSync } from 'node:fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'))
+
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
   plugins: [
     vue(),
     vueDevTools(),
